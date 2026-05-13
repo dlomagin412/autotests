cb1ab369a1b3fe435d862458d05e102c8e50f7fa
dlomagin412 <dlomagin412@mail.ru>
2026-05-13 23:28:50 +0300

    fixed bug with logout

--- SeleniumAutoTests/Helpers/LoginHelper.cs
+++ SeleniumAutoTests/Helpers/LoginHelper.cs
@@ -9,48 +9,51 @@ namespace SeleniumAutoTests.Helpers
 
         public bool IsLoggedIn()
         {
-            return driver.FindElements(By.XPath("//button[text()='Logout']")).Count > 0;
-        }
-
-        public bool IsLoggedIn(string username)
-        {
-            return IsLoggedIn();
-        }
-
-        public void Logout()
-        {
-            if (IsLoggedIn())
-            {
-                driver.FindElement(By.XPath("//button[text()='Logout']")).Click();
-                Thread.Sleep(2000);
-
-                manager.Navigation.OpenHomePage();
-                Thread.Sleep(1000);
-            }
+            return driver.FindElements(By.XPath("//button[text()='Logout']")).Count > 0
+                && driver.FindElements(By.Id("username")).Count == 0;
         }
 
         public void Login(AccountData account)
         {
             if (IsLoggedIn())
             {
-                if (IsLoggedIn(account.Username))
-                {
-                    return;
-                }
-                Logout();
+                return;
+            }
+
+            
+            if (driver.FindElements(By.XPath("//button[text()='Logout']")).Count > 0
+                && driver.FindElements(By.Id("username")).Count > 0)
+            {
+                driver.FindElement(By.XPath("//button[text()='Logout']")).Click();
+                Thread.Sleep(1000);
             }
 
             if (driver.FindElements(By.Id("username")).Count == 0)
             {
                 manager.Navigation.OpenHomePage();
-                Thread.Sleep(2000);
+                Thread.Sleep(1000);
             }
 
             FillTheField(By.Id("username"), account.Username);
             FillTheField(By.Id("password"), account.Password);
             driver.FindElement(By.Id("doLogin")).Click();
 
-            Thread.Sleep(3000);
+            int attempts = 0;
+            while (driver.FindElements(By.Id("username")).Count > 0 && attempts < 10)
+            {
+                Thread.Sleep(500);
+                attempts++;
+            }
+        }
+
+        public void Logout()
+        {
+            if (driver.FindElements(By.XPath("//button[text()='Logout']")).Count > 0)
+            {
+                driver.FindElement(By.XPath("//button[text()='Logout']")).Click();
+                Thread.Sleep(1000);
+                manager.Navigation.OpenHomePage();
+            }
         }
     }
 }
